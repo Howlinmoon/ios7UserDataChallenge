dc41fa6478d0b12f396ddc2efadd4b14663f70dc
Jim Veneskey <github@strifecrafter.com>
2015-03-27 13:05:34 -0400

    Challenge Completed

--- CCUserData.m
+++ CCUserData.m
@@ -13,13 +13,13 @@
 
 
 +(NSArray *) users {
-    NSDictionary *user1 = @{ @"username" : @"Master Photographer", @"email" : @"worldTraveler1@me.com", @"password" : @"drowssap", @"age" : @24, @"profilePicture" : [UIImage imageNamed:@"person1.jpeg"]};
+    NSDictionary *user1 = @{ USER_NAME : @"Master Photographer", USER_EMAIL : @"worldTraveler1@me.com", USER_PASSWORD : @"drowssap", USER_AGE : @24, USER_PROFILE_PICTURE : [UIImage imageNamed:@"person1.jpeg"]};
     
-    NSDictionary *user2 = @{ @"username" : @"Lots of Tots", @"email" : @"otterskipsme@me.com", @"password" : @"drowssap2", @"age" : @65, @"profilePicture" : [UIImage imageNamed:@"person2.jpeg"]};
+    NSDictionary *user2 = @{ USER_NAME : @"Lots of Tots", USER_EMAIL : @"otterskipsme@me.com", USER_PASSWORD : @"drowssap2", USER_AGE : @65, USER_PROFILE_PICTURE : [UIImage imageNamed:@"person2.jpeg"]};
     
-    NSDictionary *user3 = @{ @"username" : @"iTechie", @"email" : @"theRealApple@me.com", @"password" : @"infiteloop", @"age" : @30, @"profilePicture" : [UIImage imageNamed:@"person3.jpg"]};
+    NSDictionary *user3 = @{ USER_NAME : @"iTechie", USER_EMAIL : @"theRealApple@me.com", USER_PASSWORD : @"infiteloop", USER_AGE : @30, USER_PROFILE_PICTURE : [UIImage imageNamed:@"person3.jpg"]};
     
-    NSDictionary *user4 = @{ @"username" : @"Royal", @"email" : @"king@me.com", @"password" : @"IGotAPalace", @"age" : @0, @"profilePicture" : [UIImage imageNamed:@"person4.jpeg"]};
+    NSDictionary *user4 = @{ USER_NAME : @"Royal", USER_EMAIL : @"king@me.com", USER_PASSWORD : @"IGotAPalace", USER_AGE : @0, USER_PROFILE_PICTURE : [UIImage imageNamed:@"person4.jpeg"]};
     
     // a new method of creating an array and initializing it
     NSArray *usersArray = @[user1, user2, user3, user4];
