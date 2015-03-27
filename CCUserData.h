112ac2b23c4770243fe358318ee4669408d72e6c
Jim Veneskey <github@strifecrafter.com>
2015-03-27 12:38:00 -0400

    first commit

---
+++ CCUserData.h
@@ -0,0 +1,13 @@
+//
+//  CCUserData.h
+//  UserDataChallengeSolution
+//
+//  Created by jim Veneskey on 3/27/15.
+//  Copyright (c) 2015 Jim Veneskey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCUserData : NSObject
+
+@end
---
+++ CCUserData.m
@@ -0,0 +1,13 @@
+//
+//  CCUserData.m
+//  UserDataChallengeSolution
+//
+//  Created by jim Veneskey on 3/27/15.
+//  Copyright (c) 2015 Jim Veneskey. All rights reserved.
+//
+
+#import "CCUserData.h"
+
+@implementation CCUserData
+
+@end
